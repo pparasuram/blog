1390dd5173cd6f37c2393e371284e87bc27952e4
Parasuram, Prakash (Prakash) <pparasuram@gmail.com>
2020-03-22 15:22:25 -0400

    Chapter 7 completed

--- src/test/java/com/spankinfresh/blog/BlogPostControllerMockTests.java
+++ src/test/java/com/spankinfresh/blog/BlogPostControllerMockTests.java
@@ -16,9 +16,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.mockito.ArgumentMatchers.any;
 // import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 
 @SpringBootTest
@@ -30,10 +31,12 @@ public class BlogPostControllerMockTests {
     private final ObjectMapper mapper = new ObjectMapper();
     private static final BlogPost testPosting =
             new BlogPost(0L, "category", null, "title", "content");
+/*
     @Test
     @DisplayName("T01 - POST accepts and returns blog post representation")
     public void postCreatesNewBlogEntry_Test(@Autowired MockMvc mockMvc)
             throws Exception {
+        when(mockRepository.save(any(BlogPost.class))).thenReturn(testPosting);
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(RESOURCE_URI)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(testPosting)))
@@ -64,13 +67,28 @@ public class BlogPostControllerMockTests {
         assertEquals("http://localhost/api/articles/2",
                 mockResponse.getHeader("Location"));
     }
-    @Test
-    @DisplayName("T02 - POST automatically adds the datePosted")
-    public void test02(@Autowired MockMvc mockMvc) throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post(RESOURCE_URI)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(testPosting)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.datePosted").isNotEmpty());
-    }
+*/
+@Test
+@DisplayName("T01 - POST accepts and returns blog post representation")
+public void test01(@Autowired MockMvc mockMvc)
+        throws Exception {
+    when(mockRepository.save(any(BlogPost.class)))
+            .thenReturn(testPosting);
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(RESOURCE_URI)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(testPosting)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").value(testPosting.getId()))
+            .andExpect(jsonPath("$.title").value(testPosting.getTitle()))
+            .andExpect(
+                    jsonPath("$.category").value(testPosting.getCategory()))
+            .andExpect(
+                    jsonPath("$.content").value(testPosting.getContent()))
+            .andReturn();
+    MockHttpServletResponse mockResponse = result.getResponse();
+    assertEquals(String.format(
+            "http://localhost/api/articles/%d",testPosting.getId()),
+            mockResponse.getHeader("Location"));
+    verify(mockRepository, times(1)).save(any(BlogPost.class));
+}
 }
