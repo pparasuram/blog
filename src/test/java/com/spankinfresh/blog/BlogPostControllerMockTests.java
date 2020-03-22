3619f5016fd0435f14051cd41da15ac202d6454a
Parasuram, Prakash (Prakash) <pparasuram@gmail.com>
2020-03-22 14:32:39 -0400

    Chapter 7 started

---
+++ src/test/java/com/spankinfresh/blog/BlogPostControllerMockTests.java
@@ -0,0 +1,76 @@
+package com.spankinfresh.blog;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spankinfresh.blog.data.BlogPostRepository;
+import com.spankinfresh.blog.domain.BlogPost;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BlogPostControllerMockTests {
+    @MockBean
+    private BlogPostRepository mockRepository;
+    private static final String RESOURCE_URI = "/api/articles";
+    private final ObjectMapper mapper = new ObjectMapper();
+    private static final BlogPost testPosting =
+            new BlogPost(0L, "category", null, "title", "content");
+    @Test
+    @DisplayName("T01 - POST accepts and returns blog post representation")
+    public void postCreatesNewBlogEntry_Test(@Autowired MockMvc mockMvc)
+            throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(RESOURCE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(testPosting)))
+                .andExpect(status().isCreated())
+                .andExpect( jsonPath("$.id").value(1L))
+                .andExpect( jsonPath("$.title").value(testPosting.getTitle()))
+                .andExpect( jsonPath("$.category").value(testPosting.getCategory()))
+                .andExpect( jsonPath("$.content").value(testPosting.getContent()))
+                .andReturn();
+        MockHttpServletResponse mockResponse = result.getResponse();
+        assertEquals("http://localhost/api/articles/1",
+                mockResponse.getHeader("Location"));
+
+        // Send the second instance:
+        result = mockMvc.perform(MockMvcRequestBuilders.post(RESOURCE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(testPosting)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(2L))
+                .andExpect(
+                        jsonPath("$.title").value(testPosting.getTitle()))
+                .andExpect(
+                        jsonPath("$.category").value(testPosting.getCategory()))
+                .andExpect(
+                        jsonPath("$.content").value(testPosting.getContent()))
+                .andReturn();
+        mockResponse = result.getResponse();
+        assertEquals("http://localhost/api/articles/2",
+                mockResponse.getHeader("Location"));
+    }
+    @Test
+    @DisplayName("T02 - POST automatically adds the datePosted")
+    public void test02(@Autowired MockMvc mockMvc) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(RESOURCE_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(testPosting)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.datePosted").isNotEmpty());
+    }
+}
